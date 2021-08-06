218e5a37f306db1c9232d15f0ea36e0b5eabc53b
Vinay1089 <vinaykumarvenu100@gmail.com>
2021-08-06 15:47:11 +0530

    updated

--- Javascript/external.js
+++ Javascript/external.js
@@ -1,4 +1,5 @@
 
 document.write("<h1>External JS Defination:</h1>");
-document.write("<h3>JavaScript code are stored in separate external file using the . js extension (Ex: external. js)."+
-"In the HTML file, the (script) tag can also be used to indicate the location of a JavaScript file.</h3>");
+document.write("<h3>JavaScript code are stored in separate external file using the."+ 
+"js extension (Ex: external. js).In the HTML file, the (script) tag can also be used "+
+"to indicate the location of a JavaScript file.</h3>");

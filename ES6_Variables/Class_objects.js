a3bc18b37969ff8613d83e051611d427e7849dce
Vinay1089 <vinaykumarvenu100@gmail.com>
2021-08-18 15:41:10 +0530

    updated

---
+++ ES6_Variables/Class_objects.js
@@ -0,0 +1,19 @@
+class lenses{
+    getPortrait(){
+        console.log("85mm is the good portrait lense.");
+    }
+    getRegular(){
+        console.log("24-105mm is the good regular lense.");
+    }
+    getCandid(){
+        console.log("70-200mm is the best candid lense.");
+    }
+    getWide(){
+        console.log("14mm is the best wide lense.")
+    }
+}
+l1= new lenses();
+l1.getPortrait();
+l1.getRegular();
+l1.getCandid();
+l1.getWide();
---
+++ ES6_Variables/LET-VAR-CONST_example.js
@@ -0,0 +1,8 @@
+var firstname="Vinay Kumar";
+var firstname="Sandeep Kumar";
+let lastname="Vennu";
+lastname="Konda";
+const salary=50000;
+salary=35000;
+
+console.log("My name is"+" "+ firstname +" "+ lastname +" " +"and my salary is " + salary);

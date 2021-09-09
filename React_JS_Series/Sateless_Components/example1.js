ba50a1a09e47925ed74f62c53415eb706e0d5aa8
Vinay1089 <vinaykumarvenu100@gmail.com>
2021-09-09 20:57:22 +0530

    updated

---
+++ React_JS_Series/Sateless_Components/example1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function vinay(){
+    
+        return (
+            <div>
+                <h1> I'm learning React JS...</h1>
+            </div>
+        )
+    
+}
---
+++ React_JS_Series/Statefull_Components/example1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class vinay extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is my first introduction to React-JS</h1>
+            </div>
+        )
+    }
+}

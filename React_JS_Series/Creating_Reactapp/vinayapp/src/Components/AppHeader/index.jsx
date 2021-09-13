755c23487eadf7efc88c4e614ea0b670fc48c410
Vinay1089 <vinaykumarvenu100@gmail.com>
2021-09-13 16:35:35 +0530

    updated

---
+++ React_JS_Series/Creating_Reactapp/vinayapp/src/Components/AppHeader/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Appheader() {
+    return (
+        <div>
+            <h1>This is my first introduction to the ReactApp</h1>
+        </div>
+    )
+}

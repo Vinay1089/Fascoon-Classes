241042c7ec0c234b65dc2860bba9e108951c0480
Vinay1089 <vinaykumarvenu100@gmail.com>
2021-09-16 22:29:16 +0530

    updated

--- React_JS_Series/Creating_Reactapp/vinayapp/src/App.js
+++ React_JS_Series/Creating_Reactapp/vinayapp/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Appheader from './Components/AppHeader';
+import AppBody from './Components/AppBody';
+import AppFooter from './Components/AppFooter';
 
 function App() {
   return (
     <div className="App">
         <Appheader />
+        <AppBody />
+        <AppFooter />
     </div>
   );
 }
---
+++ React_JS_Series/Creating_Reactapp/vinayapp/src/Components/AppFooter/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Kumar() {
+    return (
+        <div>
+            <h4>Copyright 2021</h4>
+        </div>
+    )
+}

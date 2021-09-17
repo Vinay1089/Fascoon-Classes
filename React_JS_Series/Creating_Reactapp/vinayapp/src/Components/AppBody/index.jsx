763d3c0362a2286970f712209c8da38710e4366e
Vinay1089 <vinaykumarvenu100@gmail.com>
2021-09-17 17:04:27 +0530

    ouput

--- React_JS_Series/Creating_Reactapp/vinayapp/src/Components/AppBody/index.jsx
+++ React_JS_Series/Creating_Reactapp/vinayapp/src/Components/AppBody/index.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
-
 export default class vennus extends Component {
     constructor(props){
         super(props);
         this.state = {
 
             "firstname":"",
-            "lastname":""
+            "lastname":"",
+            "output":""
         }
     }
-    handleInputChange = (e) =>{
-        const target= e.target;
+    handleInputChange = (i) =>{
+        const target= i.target;
         const name= target.name;
         const value= target.value;
         this.setState({
@@ -20,12 +20,23 @@ export default class vennus extends Component {
         console.log(this.state.firstname);
         console.log(this.state.lastname);
     }
+    onOutput = (i) =>{
+        const output = parseInt(this.state.firstname) + parseInt(this.state.lastname);
+        this.setState({
+
+            output:output
+        })
+    }
     render() {
         return (
             <div>
                 <input name="firstname" type="text" onChange={this.handleInputChange} value={this.state.firstname} placeholder="First Name"/><br />
                 <input name="lastname" type="text" onChange={this.handleInputChange} value={this.state.lastname} placeholder="Last Name"/><br />
-                <button>Submit</button>
+                <button onClick={this.onOutput}>Submit</button>
+                <br /><br />
+                <h3>{this.state.firstname} {this.state.lastname}</h3>
+                <br /><br />
+                <h3>{this.state.output}</h3>
             </div>
         )
     }
